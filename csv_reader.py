39d94c98129c816123c78a0fb6bf5b3f9116a8cf
genosseredcat <theowlintheforest@gmail.com>
2021-07-06 17:18:26 +0200

    Herr Doerzbach hat mir mit dem Loop Problem geholfen. Ich arbeite nun an einem neuen Footer template

--- csv_reader.py
+++ csv_reader.py
@@ -1,37 +1,34 @@
 import csv
 
 
-
 def parserechnung(filename):
     with open(filename) as csvfile:
         csvReader = csv.reader(csvfile, delimiter=';')
 
-        rechnungsdata=dict()
+        rechnungsdata = dict()
 
-        #Write all into a row
+        # Write all into a row
 
         rechnungsdata["rechnungspos"] = []
-        for row in csvReader: #jedes row ein subarray
-            #print all rows out
-            #print(row)
+        rechnungsdata["rechnungstotal"] = 0.0
+        for row in csvReader:  # jedes row ein subarray
+            # print all rows out
+            # print(row)
 
             if row[0].startswith("Rechnung_"):
-                rechnungsdata["rechnungsNr"] = row[0][9:] #von null bis _ zählen dann ab neunter stelle alles nehmen
-                #print(rechnungsdata["rechnungsNr"])
+                rechnungsdata["rechnungsNr"] = row[0][9:]  # von null bis _ zählen dann ab neunter stelle alles nehmen
+                # print(rechnungsdata["rechnungsNr"])
 
                 rechnungsdata["auftragsnummer"] = row[1][8:]
-
 
                 rechnungsdata["ort_datum1"] = row[2]
                 rechnungsdata["ort_datum2"] = row[3]
 
-
             if row[0] == "Herkunft":
-
                 rechnungsdata["kundennummer"] = row[2]
 
                 rechnungsstellerID = row[1]
-                rechnungsstellerkundenNr=row[2]
+                rechnungsstellerkundenNr = row[2]
                 rechnungsdata["senderName"] = row[3]
                 rechnungsdata["senderAdresse"] = row[4]
                 rechnungsdata["senderPLZ"] = row[5]
@@ -44,27 +41,10 @@ def parserechnung(filename):
                 rechnungsdata["kundenPLZ"] = row[4]
                 # rechnungsdata["rechnungsNr"] = row[2][9:]   alles hinter dran ab der 9 stelle vom Feld null
 
-
-
-
-
-            rechnArray = []
             if row[0] == "RechnPos":
-                rechnArray.append(row)
-                text = []
-                for element in rechnArray: #Für alli
-                    newText = ""
-                    newText = newText + "  "
-                    newText = newText + element[1]
-                    newText = newText + "   "
-                    newText = newText + element[2]
-                    newText = newText + "      " + element[3]
-                    newText = newText + "    " + element[4] + " CHF"
-                    newText = newText + "     " + element[5]
-                    newText = newText + element[6]
-                    text.append(newText)
+                rechnungsdata["rechnungspos"].append(row)
+                rechnungsdata["rechnungstotal"] += float(row[5])
 
-                    print(text)
-                    rechnungsdata["rechnungspos"] = text  #Contains both in one
+
 
         return rechnungsdata
--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from csv_reader import parserechnung
 rechnungsdata=parserechnung("rechnung21003.data")
 os.chdir("..") #Mussi no schönner mache gfallt ihm ned so
 
+#speichern als output file name steht in auftrag
+with open("template_head.txt") as tplfle:
+    tpl = tplfle.read()
+    print(tpl % rechnungsdata) #statt print write
 
-with open("template.txt") as tplfle:
+with open("template_rechnungspos.txt") as tplfile_rechnungspos:
+    tpl_pos = tplfile_rechnungspos.read()
+    for rechnungspos in rechnungsdata["rechnungspos"]:
+        print(tpl_pos % (rechnungspos[1],rechnungspos[2],rechnungspos[3],float(rechnungspos[4]),float(rechnungspos[5])))
+
+with open("template_footer.txt") as tplfle:
     tpl = tplfle.read()
     print(tpl % rechnungsdata)
-
